bdff506d47c7643ed9f1008ea6ee9122a357a9fe
Jeremy Hull <sourdrums@gmail.com>
2019-11-20 14:29:31 -0700

    Move markdown to react into utility

---
+++ utility/index.js
@@ -0,0 +1,3 @@
+export { default as fetchAPI }        from './fetchAPI';
+export { default as markdownToReact } from './markdownToReact';
+export { default as trackerEvent }    from './trackerEvent';
---
+++ utility/markdownToReact.js
@@ -0,0 +1,28 @@
+import extend     from 'lodash/extend';
+import htmlParser from 'html-react-parser';
+import marked     from 'marked';
+
+function isAnchorTag(node) {
+    return node.type === 'tag' && node.name === 'a';
+}
+
+function handleAnchorTagReplace(node) {
+    if (!isAnchorTag(node)) {
+        return node;
+    }
+
+    node.attribs = extend({}, node.attribs, {
+        rel: 'noopener noreferrer',
+        target: '__blank'
+    });
+
+    return node;
+}
+
+function markdownToReact(content) {
+    const html = marked(content);
+
+    return htmlParser(html, { replace: handleAnchorTagReplace });
+}
+
+export default markdownToReact;
