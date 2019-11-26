f975e83f03fe12ffe2239f0888ffd44434fa732a
Jeremy Hull <sourdrums@gmail.com>
2019-11-26 00:24:28 -0700

    Export new utilities in index file

--- utility/index.js
+++ utility/index.js
@@ -1,3 +1,5 @@
+export { default as createCache }     from './cache';
 export { default as fetchAPI }        from './fetchAPI';
 export { default as markdownToReact } from './markdownToReact';
+export { default as toQueryString }   from './toQueryString';
 export { default as trackerEvent }    from './trackerEvent';
