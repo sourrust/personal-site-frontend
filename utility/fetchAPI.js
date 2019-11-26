edac864202bdc6030cbf75bb6fc3d06405889f75
Jeremy Hull <sourdrums@gmail.com>
2019-11-26 00:24:05 -0700

    Add cache store to fetchAPI

--- utility/fetchAPI.js
+++ utility/fetchAPI.js
@@ -1,14 +1,23 @@
 import axios   from 'axios';
 import isEmpty from 'lodash/isEmpty';
 
-async function fetchAPI(routeUrl, options) {
-    const url = process.env.BASE_CMS_URL + routeUrl;
+import createCache from './cache';
+
+const cache = createCache();
+
+async function fetchAPI(routeUrl, initialOptions) {
+    const url     = process.env.BASE_CMS_URL + routeUrl;
+    const options = initialOptions || {};
+
+    if (cache.has(routeUrl, options)) {
+        return cache.get(routeUrl, options);
+    }
 
     const response = isEmpty(options)
         ? await axios.get(url)
         : await axios(url, options);
 
-    return response.data;
+    return cache.set(routeUrl, options, response.data);
 }
 
 export default fetchAPI;
