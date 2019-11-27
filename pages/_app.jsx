4d5831112091983c2bbfc076a1bd7f8323820b1b
Jeremy Hull <sourdrums@gmail.com>
2019-11-27 01:57:06 -0700

    Add router event handler to track page views

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,21 @@
-import App   from 'next/app';
-import Head  from 'next/head';
-import React from 'react';
+import App    from 'next/app';
+import Head   from 'next/head';
+import React  from 'react';
+import Router from 'next/router';
+
+import pageView from '../utility/pageView';
 
 import '../scss/index.scss';
 
 class BaseApplication extends App {
+    componentDidMount() {
+        Router.events.on('routeChangeStart', pageView);
+    }
+
+    componentWillUnmount() {
+        Router.events.off('routeChangeStart', pageView);
+    }
+
     render() {
         const { Component, pageProps } = this.props;
 
