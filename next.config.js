188c0dd7ee546caf00e9aff7c3296ecc83042716
Jeremy Hull <sourdrums@gmail.com>
2019-12-31 22:36:02 -0700

    Add getBaseUrl for different machine environments

--- next.config.js
+++ next.config.js
@@ -5,15 +5,48 @@ const withSass = require('@zeit/next-sass');
 require('dotenv').config();
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
-const baseUrl = isDevelopment
-    ? 'http://localhost:1337'
-    : 'https://cms.jeremy-hull.com';
+
+function getDevelopmentBaseUrl() {
+    const result    = {};
+    const localhost = 'http://localhost:1337';
+
+    if (process.env.MACHINE_ENV === 'docker') {
+        result.server = 'http://personal-site.backend:1337';
+        result.client = localhost;
+    } else {
+        result.server = localhost;
+        result.client = localhost;
+    }
+
+    return result;
+}
+
+function getProductionBaseUrl() {
+    const result  = {};
+    const backend = 'https://cms.jeremy-hull.com';
+
+    if (process.env.MACHINE_ENV === 'docker') {
+        result.server = 'http://personal-site.backend:80';
+        result.client = backend;
+    } else {
+        result.server = backend;
+        result.client = backend;
+    }
+
+    return result;
+}
+
+function getBaseUrl() {
+    return isDevelopment
+        ? getDevelopmentBaseUrl()
+        : getProductionBaseUrl();
+}
 
 module.exports = withSass({
     poweredByHeader: false,
     env: {
         GOOGLE_ANALYTICS_ID: process.env.GOOGLE_ANALYTICS_ID,
-        BASE_CMS_URL: baseUrl
+        BASE_CMS_URL: getBaseUrl()
     },
     sassLoaderOptions: {
         includePaths: ['node_modules'],
