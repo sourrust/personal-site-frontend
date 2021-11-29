08dbea20f3bacb32a0da85f17e93ffcf9b133138
Jeremy Hull <sourdrums@gmail.com>
2021-11-28 20:07:00 -0700

    Update to using typescript

---
+++ src/pages/api/contact.ts
@@ -0,0 +1,126 @@
+import Celebrate  from 'celebrate';
+import http       from 'http';
+import isEmpty    from 'lodash/isEmpty';
+import nodemailer from 'nodemailer';
+
+import { NextApiResponse, NextApiRequest } from 'next';
+
+import runApiMiddleware from '../../utility/runApiMiddleware';
+
+interface Message {
+    name: string;
+    email: string;
+    subject?: string;
+    message: string;
+}
+
+function getTestConfiguration() {
+    const authorization = {
+        user: 'austin30@ethereal.email',
+        pass: 'Gb5tyjdyqmYV1bud4J'
+    };
+
+    return {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: authorization
+    };
+}
+
+function getConfiguration() {
+    const authorization = {
+        user: process.env.GMAIL_USERNAME,
+        pass: process.env.GMAIL_PASSWORD
+    };
+
+    return {
+        host: 'smtp.gmail.com',
+        post: 465,
+        secure: true,
+        auth: authorization
+    };
+}
+
+function getDefaultSubject(name: string, subject?: string) {
+    if (isEmpty(subject)) {
+        return `[personal-site] Message from "${name}"`;
+    }
+
+    return `[personal-site] Message from "${name}" about "${subject}"`;
+}
+
+function sendMessage(body: Message) {
+    const {
+        name, email, subject, message
+    } = body;
+
+    const defaultName    = name || 'Anonymous';
+    const defaultSubject = getDefaultSubject(name, subject);
+
+    const transporter = nodemailer.createTransport(
+        true
+            ? getConfiguration()
+            : getTestConfiguration()
+    );
+
+    return transporter.sendMail({
+        to: `"Jeremy Hull" <${process.env.EMAIL_USERNAME}>`,
+        bcc: `"Jeremy Hull" <${process.env.GMAIL_USERNAME}>`,
+        replyTo: email,
+        subject: getDefaultSubject(defaultName, defaultSubject),
+        text: message
+    });
+}
+
+const { Joi, Segments } = Celebrate;
+
+const validation = Celebrate.celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().default('Anonymous'),
+        email: Joi.string().email().required(),
+        subject: Joi.string(),
+        message: Joi.string().required()
+    })
+});
+
+function handleCelebrateMiddleware(request: NextApiRequest, response: NextApiResponse) {
+    return runApiMiddleware(request, response, validation)
+        .catch((error) => runApiMiddleware(error, request, response, Celebrate.errors()));
+}
+
+async function handler(request: NextApiRequest, response: NextApiResponse) {
+    if (request.method !== 'POST') {
+        response.status(404).json({
+            statusCode: 404,
+            error: http.STATUS_CODES[404],
+            message: 'Can not find handler for this method type'
+        });
+
+        return;
+    }
+
+    await handleCelebrateMiddleware(request, response);
+
+    let payload = null;
+
+    try {
+        payload = await sendMessage(request.body);
+    } catch (error) {
+        console.error(error);
+
+        response.status(500).json({
+            statusCode: 500,
+            error: http.STATUS_CODES[500],
+            message: (error as Error).message || 'An unknown error has occured on the server'
+        });
+
+        return;
+    }
+
+    response.json({
+        statusCode: 200,
+        payload: payload
+    });
+}
+
+export default handler;
